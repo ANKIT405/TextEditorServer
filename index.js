4ba0a409fca73d6bcae22d3221c97177aebc598a
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-04-11 18:59:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let server = http.createServer(app); // step2:createServer by HTTP module and gi
 let io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
+  methods: ["POST",GET]
   },
 }); //step3:import the socket.io and call and give argument server that was created above
 
