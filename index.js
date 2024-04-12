9906452c2695255b2c72008977e67b0e74d71a02
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-04-12 21:17:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let cors = require("cors");
 let express = require("express"); //step1:import express
 
-let hostname=0.0.0.0
+// let hostname=0.0.0.0
 
- let Port=null
+//  let Port=null
 
 let app = express();
 let http = require("http");
@@ -63,6 +63,4 @@ app.route("/").get((req, res) => {
   res.send("SERVER");
 });
 
-server.listen(Port,hostname,() => {
-  console.log("Server is listening at Port 5000");
-});
+server.listen("https://text-editor-server.vercel.app");
