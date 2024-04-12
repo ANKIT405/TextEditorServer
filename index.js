0a1802ca085fdeaea19a6341e1845b714de0d179
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-04-12 22:11:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let server = http.createServer(app); // step2:createServer by HTTP module and gi
 app.use(cors())
 let io = require("socket.io")(server, {
   cors: {
-    origin: "https://super-txt.netlify.app",
+    origin: "https://hero-editor.netlify.app",
      methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
      credentials: true
